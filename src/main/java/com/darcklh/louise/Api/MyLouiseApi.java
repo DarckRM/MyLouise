08525e34516fa5a6594b4d643c15eb49a29624b9
DarckRM <412543224@qq.com>
2021-08-05 00:09:15 +0800

    实现随机setu接口

--- src/main/java/com/darcklh/louise/Api/MyLouiseApi.java
+++ src/main/java/com/darcklh/louise/Api/MyLouiseApi.java
@@ -1,5 +1,6 @@
 package com.darcklh.louise.Api;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
+@RequestMapping("/louise")
 public class MyLouiseApi {
     Logger logger = LoggerFactory.getLogger(MyLouiseApi.class);
 
@@ -69,12 +71,45 @@ public class MyLouiseApi {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers= new HttpHeaders();
         JSONObject jsonObject = new JSONObject();
+
+        //请求LoliApi V2获得随机SETU
+        HttpHeaders loli = new HttpHeaders();
+        loli.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject requestLoli = new JSONObject();
+        requestLoli.put("size","small");
+
+        HttpEntity<String> littlLoli = new HttpEntity<>(requestLoli.toString(), loli);
+        String result = restTemplate.postForObject("https://api.lolicon.app/setu/v2", littlLoli, String.class);
+        JSONObject goodLoli = JSONObject.parseObject(result);
+        System.out.println(goodLoli);
+        //格式化结果
+        String title = "";
+        String author = "";
+        String pid = "";
+        String url = "";
+        JSONArray loliArray = goodLoli.getJSONArray("data");
+
+        for (int i = 0; i < loliArray.size(); i++) {
+            title = loliArray.getJSONObject(i).getString("title");
+            author = loliArray.getJSONObject(i).getString("author");
+            pid = loliArray.getJSONObject(i).getString("pid");
+            url = loliArray.getJSONObject(i).getJSONObject("urls").getString("small");
+        }
+
         if (operate == 1) {
             jsonObject.put("user_id",id);
-            jsonObject.put("message","[CQ:image,file=https://iw233.cn/API/Random.php]");
+            jsonObject.put("message",
+                    "标题:"+title+
+                    "\n作者:"+author+
+                    "\npid:"+pid+
+                    "\n[CQ:image,file="+url+"]");
         } else if (operate == 2) {
             jsonObject.put("group_id",id);
-            jsonObject.put("message","[CQ:image,file=https://iw233.cn/API/Random.php]");
+            jsonObject.put("message",
+                    "标题:"+title+
+                    "\n作者:"+author+
+                    "\npid:"+pid+
+                    "\n[CQ:image,file="+url+"]");
         }
 
 
