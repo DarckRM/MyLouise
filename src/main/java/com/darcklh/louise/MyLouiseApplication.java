202625c0954fd69cf66fb18b0f97c8ef629567a7
DarckRM <412543224@qq.com>
2021-08-04 21:12:03 +0800

    项目初提交

---
+++ src/main/java/com/darcklh/louise/MyLouiseApplication.java
@@ -0,0 +1,13 @@
+package com.darcklh.louise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLouiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLouiseApplication.class, args);
+    }
+
+}
