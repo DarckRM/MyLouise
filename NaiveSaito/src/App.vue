ea7ce7f8d8945c7279d8c7d1a996f89d7ea5e053
DarckRM <412543224@qq.com>
2021-09-10 13:08:32 +0800

    2021年9月10日13:08:20 DarckLH VUE NaiveUI 前端

---
+++ NaiveSaito/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue';
+</script>
+
+<template>
+<img style="width: 150px" alt="Vue logo" src="./assets/logo.png">
+MyLouise Backfront Alpha
+<n-divider />
+<LoginPage></LoginPage>
+</template>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
+
+<script>
+import { defineComponent } from 'vue'
+import { NDivider } from 'naive-ui'
+
+export default defineComponent({
+  components: {
+    NDivider
+  }
+})
+</script>
+
---
+++ NaiveSaito/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <n-button>naive-ui</n-button>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { NButton } from 'naive-ui'
+
+  export default defineComponent({
+    components: {
+      NButton
+    }
+  })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
