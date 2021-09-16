b284f0e442590753ad5b0cf03ef84789ee216a29
DarckRM <412543224@qq.com>
2021-09-16 11:41:14 +0800

    2021年9月16日11:40:55 DarckLH 前端主页初步搭建

---
+++ NaiveSaito/src/components/home/Menu.vue
@@ -0,0 +1,97 @@
+<template>
+    <n-menu
+    :options="menuOptions"
+    :root-indent="36"
+    :indent="12"
+    :default-expanded-keys="defaultExpandedKeys"
+    />
+</template>
+
+<script>
+import { defineComponent, h } from 'vue'
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+  CogOutline as CogIcon
+} from '@vicons/ionicons5'
+import  { useMessage, NIcon } from 'naive-ui'
+
+export default defineComponent({
+  setup () {
+    const message = useMessage()
+    return {
+      menuOptions,
+      defaultExpandedKeys: ['saito-sys-config', 'louise-sys-config'],
+    }
+  }
+})
+
+function renderIcon (icon) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: 'Louise系统配置',
+    key: 'louise-sys-config',
+    icon: renderIcon(CogIcon),
+    children: [
+        {
+            type: 'group',
+            label: '基础项',
+            key: 'louise-basic',
+            children: [
+                {
+
+                }
+            ]
+        }
+    ]
+  },
+  {
+    label: 'Saito系统配置   ',
+    key: 'saito-sys-config',
+    icon: renderIcon(BookIcon),
+    children: [
+      {
+        type: 'group',
+        label: '基础项',
+        key: 'people',
+        children: [
+          {
+            label: '叙事者',
+            key: 'narrator',
+            icon: renderIcon(PersonIcon)
+          },
+          {
+            label: '羊男',
+            key: 'sheep-man',
+            icon: renderIcon(PersonIcon)
+          }
+        ]
+      },
+      {
+        label: '饮品',
+        key: 'beverage',
+        icon: renderIcon(WineIcon),
+        children: [
+          {
+            label: '威士忌',
+            key: 'whisky'
+          }
+        ]
+      },
+      {
+        label: '过去增多，未来减少',
+        key: 'the-past-increases-the-future-recedes'
+      }
+    ]
+  }
+]
+
+</script>
+
+<style>
+
+</style>
---
+++ NaiveSaito/src/components/home/Top.vue
@@ -0,0 +1,57 @@
+<template>
+    <n-menu
+    :options="menuOptions"
+    mode="horizontal"
+    style="margin: auto 0"
+    ></n-menu>
+</template>
+
+<script>
+import { defineComponent, h, ref } from 'vue'
+import { NIcon } from 'naive-ui'
+import {
+  BookOutline as BookIcon,
+} from '@vicons/ionicons5'
+
+export default defineComponent({
+  setup () {
+    return {
+      activeKey: ref(null),
+      menuOptions
+    }
+  }
+})
+
+function renderIcon (icon) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: () =>
+      h(
+        'a',
+        {
+          href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F',
+          target: '_blank',
+          rel: 'noopenner noreferrer'
+        },
+        '首页'
+      ),
+    key: 'hear-the-wind-sing',
+  },
+  {
+    label: '文档',
+    key: 'pinball-1973',
+  },
+  {
+    label: '控制台',
+    key: 'a-wild-sheep-chase',
+  }
+]
+
+</script>
+
+<style>
+
+</style>
