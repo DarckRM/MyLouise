e3a1c92647b3a7037292ead531923f28f7b9fc77
DarckRM <412543224@qq.com>
2022-04-13 21:29:51 +0800

    feature: accomplished some basic Gelbooru request feature

--- src/test/java/com/darcklh/louise/MyLouiseApplicationTests.java
+++ src/test/java/com/darcklh/louise/MyLouiseApplicationTests.java
@@ -3,12 +3,20 @@ package com.darcklh.louise;
 import com.darcklh.louise.Model.R;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MyLouiseApplicationTests {
-
 }
