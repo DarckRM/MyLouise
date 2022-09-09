81844866d0642a4f2a5e62cd076be82f918161fe
DarckRM <412543224@qq.com>
2022-09-10 07:04:24 +0800

    feature: some version control page for front-end

--- NaiveSaito/src/components/FeatureCard.vue
+++ NaiveSaito/src/components/FeatureCard.vue
@@ -1,116 +1,128 @@
 <template>
-    <n-card>
-        <n-form label-placement="left" size="medium" label-width="100">
-            <n-grid x-gap=16 :cols="5">
-                <n-gi span=1>
-                  <div style="margin: 0 auto">
-                    <n-image object-fit="cover" round :src="model.avatar" width="200" height="200" style="border-radius: 200px;"/>
-                  </div>
-                  <div style="float: left">
-                    <n-button type="primary" style="width: 105px; height: 40px; margin: 5px 50px" @click="saveFormData">保存</n-button>
-                  </div>      
-                </n-gi>
-                <n-gi span=2>
-                    <n-form-item label="功能名称">
-                        <n-input placeholder="请输入功能名称" v-model:value="model.feature_name"/>
-                    </n-form-item>
-                    <n-form-item label="调用命令">
-                        <n-input placeholder="请输入调用命令" v-model:value="model.feature_cmd">
-                        </n-input>
-                    </n-form-item>
-                    <n-form-item label="URL">
-                        <n-input placeholder="请输入调用URL" v-model:value="model.feature_url"/>
-                    </n-form-item>
-                    <n-space>
-                      <n-form-item label="CREDIT 消耗">
-                        <n-input-number v-model:value="model.credit_cost" size="small" />
-                      </n-form-item>
-                      <n-form-item label="是否鉴权">
-                        <n-switch
-                          v-model:value="model.is_auth"
-                          :default-value=1
-                          :checked-value=1
-                          :unchecked-value=0
-                        />
-                      </n-form-item>
-                    </n-space>
-                </n-gi>
-                <n-gi span=2>
-                    <n-form-item label="功能一句话">
-                        <n-input placeholder="请输入一句话" v-model:value="model.description">
-                        </n-input>
-                    </n-form-item>
-                    <n-form-item label="详细说明">
-                        <n-input
-                            placeholder="Textarea"
-                            v-model:value="model.info"
-                            type="textarea"
-                            :autosize="{
-                            minRows: 3,
-                            maxRows: 5
-                            }"
-                        />
-                    </n-form-item>
-                    <n-form-item label="周期调用限制">
-                      <n-input placeholder="[周期]/[次数]" v-model:value="model.invoke_limit">
-                      </n-input>
-                    </n-form-item>
-                    <n-form-item label="功能类型">
-                      <n-radio default-checked :checked="model.is_original === 1" value=1>源自系统</n-radio>
-                      <n-radio :checked="model.is_original === 0" value=0>外部插件</n-radio>
-                    </n-form-item>
-                </n-gi>
-            </n-grid>
-        </n-form>
-    </n-card>
+  <n-card>
+    <n-form label-placement="left" size="medium" label-width="100">
+      <n-grid x-gap="16" :cols="5">
+        <n-gi span="1">
+          <div style="margin: 0 auto">
+            <n-image
+              object-fit="cover"
+              round
+              :src="model.avatar"
+              width="200"
+              height="200"
+              style="border-radius: 200px"
+            />
+          </div>
+          <div style="float: left">
+            <n-button
+              type="primary"
+              style="width: 105px; height: 40px; margin: 5px 50px"
+              @click="saveFormData"
+              >保存</n-button
+            >
+          </div>
+        </n-gi>
+        <n-gi span="2">
+          <n-form-item label="功能名称">
+            <n-input placeholder="请输入功能名称" v-model:value="model.feature_name" />
+          </n-form-item>
+          <n-form-item label="调用命令">
+            <n-input placeholder="请输入调用命令" v-model:value="model.feature_cmd">
+            </n-input>
+          </n-form-item>
+          <n-form-item label="URL">
+            <n-input placeholder="请输入调用URL" v-model:value="model.feature_url" />
+          </n-form-item>
+          <n-space>
+            <n-form-item label="CREDIT 消耗">
+              <n-input-number v-model:value="model.credit_cost" size="small" />
+            </n-form-item>
+            <n-form-item label="是否鉴权">
+              <n-switch
+                v-model:value="model.is_auth"
+                :default-value="1"
+                :checked-value="1"
+                :unchecked-value="0"
+              />
+            </n-form-item>
+          </n-space>
+        </n-gi>
+        <n-gi span="2">
+          <n-form-item label="功能一句话">
+            <n-input placeholder="请输入一句话" v-model:value="model.description">
+            </n-input>
+          </n-form-item>
+          <n-form-item label="详细说明">
+            <n-input
+              placeholder="Textarea"
+              v-model:value="model.info"
+              type="textarea"
+              :autosize="{
+                minRows: 3,
+                maxRows: 5,
+              }"
+            />
+          </n-form-item>
+          <n-form-item label="周期调用限制">
+            <n-input placeholder="[周期]/[次数]" v-model:value="model.invoke_limit">
+            </n-input>
+          </n-form-item>
+          <n-form-item label="功能类型">
+            <n-radio default-checked :checked="model.is_original === 1" value="1"
+              >源自系统</n-radio
+            >
+            <n-radio :checked="model.is_original === 0" value="0">外部插件</n-radio>
+          </n-form-item>
+        </n-gi>
+      </n-grid>
+    </n-form>
+  </n-card>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { useMessage } from 'naive-ui'
+import { defineComponent } from "vue";
+import { useMessage } from "naive-ui";
 
 export default defineComponent({
-    setup() {
-        const message = useMessage()
-        return {
-          message
+  setup() {
+    const message = useMessage();
+    return {
+      message,
+    };
+  },
+  props: {
+    data: {},
+    width: String,
+    type: String,
+  },
+  data() {
+    return {
+      model: this.data,
+    };
+  },
+  methods: {
+    saveFormData() {
+      let formData = {
+        feature_id: this.model.feature_id,
+        feature_name: this.model.feature_name,
+        feature_cmd: this.model.feature_cmd,
+        feature_url: this.model.feature_url,
+        info: this.model.info,
+        description: this.model.description,
+        invoke_limit: this.model.invoke_limit,
+        is_original: this.model.is_original,
+        is_auth: this.model.is_auth,
+        credit_cost: this.model.credit_cost,
+      };
+      this.$axios.post("feature-info/" + this.type, formData).then((result) => {
+        let data = result.data;
+        if (data.code == 200) {
+          this.message.success(data.msg);
+        } else {
+          this.message.error(data.msg);
         }
+      });
     },
-    props: {
-        data: {
-        },
-        width: String,
-        type: String
-    },
-    data() {
-        return {
-            model: this.data
-        }
-
-    },
-    methods: {
-      saveFormData() {
-        let formData = {
-          feature_id: this.model.feature_id,
-          feature_name: this.model.feature_name,
-          feature_cmd: this.model.feature_cmd,
-          feature_url: this.model.feature_url,
-          info: this.model.info,
-          description: this.model.description,
-          invoke_limit: this.model.invoke_limit,
-          is_original: this.model.is_original,
-          is_auth: this.model.is_auth,
-          credit_cost: this.model.credit_cost
-        }
-        this.$axios.post('feature-info/' + this.type, formData).then(result => {
-          let data = result.data
-          if (data.code == 200) {
-            this.message.success(data.msg)
-          } else {
-            this.message.error(data.msg)
-          }
-        })
-      }
-    }
-})
+  },
+});
 </script>
--- NaiveSaito/src/components/websocket/WebSocket.vue
+++ NaiveSaito/src/components/websocket/WebSocket.vue
@@ -1,127 +1,122 @@
-<template>
-</template>
+<template></template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-const websocket_url = import.meta.env.VITE_WEBSOCKET_URL
+const websocket_url = import.meta.env.VITE_WEBSOCKET_URL;
 
 export default defineComponent({
-    props: {
-        client_name: '',
-        data: ''
-    },
-    name: 'WebSocket',
-    setup(props) {
-        const client_name = props.client_name
-        const data = props.data
-        return {
-            client_name,
-            data
-        }
-    },
-    data() {
-      return {
-        // 连接状态
-        isConn: false,
-        // ws是否启动
-        wsIsRun: false,
-        // 定义ws对象
-        webSocket: null,
-        // ws请求链接（类似于ws后台地址）
-        ws: websocket_url + this.client_name,
-        // ws定时器
-        wsTimer: null,
+  props: {
+    client_name: "",
+    data: "",
+  },
+  name: "WebSocket",
+  setup(props) {
+    const client_name = props.client_name;
+    const data = props.data;
+    return {
+      client_name,
+      data,
+    };
+  },
+  data() {
+    return {
+      // 连接状态
+      isConn: false,
+      // ws是否启动
+      wsIsRun: false,
+      // 定义ws对象
+      webSocket: null,
+      // ws请求链接（类似于ws后台地址）
+      ws: websocket_url + this.client_name,
+      // ws定时器
+      wsTimer: null,
+    };
+  },
+  async mounted() {
+    this.wsIsRun = true;
+    this.wsInit();
+  },
+  methods: {
+    sendDataToServer() {
+      if (this.webSocket.readyState === 1) {
+        this.webSocket.send("来自前端的数据");
+      } else {
+        throw Error("服务未连接");
       }
-    },    
-    async mounted() {
-        this.wsIsRun = true
-        this.wsInit()
     },
-    methods: {
-        sendDataToServer() {
+    /**
+     * 初始化ws
+     */
+    wsInit() {
+      if (!this.wsIsRun) return;
+      // 销毁ws
+      this.wsDestroy();
+      // 初始化ws
+      this.webSocket = new WebSocket(this.ws);
+      // ws连接建立时触发
+      this.webSocket.addEventListener("open", this.wsOpenHanler);
+      // ws服务端给客户端推送消息
+      this.webSocket.addEventListener("message", this.wsMessageHanler);
+      // ws通信发生错误时触发
+      this.webSocket.addEventListener("error", this.wsErrorHanler);
+      // ws关闭时触发
+      this.webSocket.addEventListener("close", this.wsCloseHanler);
 
-            if (this.webSocket.readyState === 1) {
-                this.webSocket.send('来自前端的数据')
-            } else {
-                throw Error('服务未连接')
-            }
-        }, 
-        /**
-         * 初始化ws
-         */
-        wsInit() {
-            
-            if (!this.wsIsRun) return
-            // 销毁ws
-            this.wsDestroy()
-            // 初始化ws
-            this.webSocket = new WebSocket(this.ws)
-            // ws连接建立时触发
-            this.webSocket.addEventListener('open', this.wsOpenHanler)
-            // ws服务端给客户端推送消息
-            this.webSocket.addEventListener('message', this.wsMessageHanler)
-            // ws通信发生错误时触发
-            this.webSocket.addEventListener('error', this.wsErrorHanler)
-            // ws关闭时触发
-            this.webSocket.addEventListener('close', this.wsCloseHanler)
-
-            // 检查ws连接状态,readyState值为0表示尚未连接，1表示建立连接，2正在关闭连接，3已经关闭或无法打开
-            clearInterval(this.wsTimer)
-            this.wsTimer = setInterval(() => {
-                if (this.webSocket.readyState === 1) {
-                    this.isConn = true
-                    clearInterval(this.wsTimer)
-                } else {
-                    this.isConn = false
-                    console.log('ws建立连接失败')
-                    this.wsInit()
-                }
-            }, 3000)
-        },
-        wsOpenHanler(event) {
-          this.isConn = true
-          console.log('ws客户端:' + this.client_name + '建立连接成功')
-        },
-        wsMessageHanler(e) {
-            const redata = JSON.parse(e.data)
-            this.data = redata.result
-        },
-        /**
-         * ws通信发生错误
-         */
-        wsErrorHanler(event) {
-            this.isConn = false
-            console.log(event, 'ws客户端:' + this.client_name + '通信发生错误')
-            this.wsInit()
-        },
-        /**
-         * ws关闭
-         */
-        wsCloseHanler(event) {
-            this.isConn = false
-            console.log('ws客户端:' + this.client_name + '关闭')
-            this.wsDestroy()
-        },
-        /**
-         * 销毁ws
-         */
-        wsDestroy() {
-            if (this.webSocket !== null) {
-                this.webSocket.removeEventListener('open', this.wsOpenHanler)
-                this.webSocket.removeEventListener('message', this.wsMessageHanler)
-                this.webSocket.removeEventListener('error', this.wsErrorHanler)
-                this.webSocket.removeEventListener('close', this.wsCloseHanler)
-                this.webSocket.close()
-                this.webSocket = null
-                clearInterval(this.wsTimer)
-            }
-        },
+      // 检查ws连接状态,readyState值为0表示尚未连接，1表示建立连接，2正在关闭连接，3已经关闭或无法打开
+      clearInterval(this.wsTimer);
+      this.wsTimer = setInterval(() => {
+        if (this.webSocket.readyState === 1) {
+          this.isConn = true;
+          clearInterval(this.wsTimer);
+        } else {
+          this.isConn = false;
+          console.log("ws建立连接失败");
+          this.wsInit();
+        }
+      }, 3000);
     },
-    unmounted() {
-      this.wsCloseHanler()
+    wsOpenHanler(event) {
+      this.isConn = true;
+      console.log("ws客户端:" + this.client_name + "建立连接成功");
     },
-
-})
-
+    wsMessageHanler(e) {
+      const redata = JSON.parse(e.data);
+      this.data = redata.result;
+    },
+    /**
+     * ws通信发生错误
+     */
+    wsErrorHanler(event) {
+      this.isConn = false;
+      console.log(event, "ws客户端:" + this.client_name + "通信发生错误");
+      this.wsInit();
+    },
+    /**
+     * ws关闭
+     */
+    wsCloseHanler(event) {
+      this.isConn = false;
+      console.log("ws客户端:" + this.client_name + "关闭");
+      this.wsDestroy();
+    },
+    /**
+     * 销毁ws
+     */
+    wsDestroy() {
+      if (this.webSocket !== null) {
+        this.webSocket.removeEventListener("open", this.wsOpenHanler);
+        this.webSocket.removeEventListener("message", this.wsMessageHanler);
+        this.webSocket.removeEventListener("error", this.wsErrorHanler);
+        this.webSocket.removeEventListener("close", this.wsCloseHanler);
+        this.webSocket.close();
+        this.webSocket = null;
+        clearInterval(this.wsTimer);
+      }
+    },
+  },
+  unmounted() {
+    this.wsCloseHanler();
+  },
+});
 </script>
